d051226a7b6393677ad7ed085b17f497f8bfa86b
Peter Kraft <pkraft@college.harvard.edu>
2014-11-28 13:34:52 -0500

    added contacts page

---
+++ NCRC/ContactsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactsTableViewController.h
+//  NCRC
+//
+//  Created by Peter Kraft on 11/28/14.
+//  Copyright (c) 2014 HCURA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsTableViewController : UITableViewController
+
+@end
---
+++ NCRC/ContactsTableViewController.m
@@ -0,0 +1,118 @@
+//
+//  ContactsTableViewController.m
+//  NCRC
+//
+//  Created by Peter Kraft on 11/28/14.
+//  Copyright (c) 2014 HCURA. All rights reserved.
+//
+
+#import "ContactsTableViewController.h"
+
+@interface ContactsTableViewController ()
+
+@end
+
+@implementation ContactsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.navigationController.navigationBar.hidden = NO;
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)HUPD_call:(id)sender {
+    //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"tel:%617495121"]];
+        //[[UIApplication sharedApplication] openURL:[NSURL URLWithString: @"http://www.ncrc.hcura.org"]];
+    NSURL *phoneNumberURL = [NSURL URLWithString:@"tel:617495121"];
+    [[UIApplication sharedApplication] openURL:phoneNumberURL];
+}
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+//#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 3;
+}
+
+
+-(void) viewWillDisappear:(BOOL)animated {
+    if ([self.navigationController.viewControllers indexOfObject:self]==NSNotFound) {
+        self.navigationController.navigationBar.hidden = YES;
+        // back button was pressed.  We know this is true because self is no longer
+        // in the navigation stack.
+    }
+    [super viewWillDisappear:animated];
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
