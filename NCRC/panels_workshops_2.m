1b7609fa1fdbb3239df38a4851c214f1e1adcdd1
Peter Kraft <pkraft@college.harvard.edu>
2015-01-02 20:09:25 -0600

    created panel/workshop placeholders

---
+++ NCRC/panels_workshops_2.m
@@ -0,0 +1,58 @@
+//
+//  panels_workshops_2.m
+//  NCRC
+//
+//  Created by Peter Kraft on 1/1/15.
+//  Copyright (c) 2015 HCURA. All rights reserved.
+//
+
+#import "panels_workshops_2.h"
+
+@interface panels_workshops_2 ()
+
+@end
+
+@implementation panels_workshops_2
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [_viewWeb addConstraints:[NSLayoutConstraint
+                              constraintsWithVisualFormat:@"H:[_viewWeb(==200)]"
+                              options:NSLayoutFormatDirectionLeadingToTrailing
+                              metrics:nil
+                              views:NSDictionaryOfVariableBindings(_viewWeb)]];
+    //create the string
+    NSMutableString *html = [NSMutableString stringWithString: @"PLACEHOLDER2"];
+    [_viewWeb loadHTMLString:[html description] baseURL:nil];
+    _viewWeb.delegate = self;
+}
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)theWebView
+{
+    CGSize contentSize = theWebView.scrollView.contentSize;
+    CGSize viewSize = self.view.bounds.size;
+    
+    float rw = viewSize.width / contentSize.width;
+    
+    theWebView.scrollView.minimumZoomScale = rw;
+    theWebView.scrollView.maximumZoomScale = rw;
+    theWebView.scrollView.zoomScale = rw;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ panels_workshops_1.h
@@ -0,0 +1,14 @@
+//
+//  panels_workshops_1.h
+//  NCRC
+//
+//  Created by Peter Kraft on 1/1/15.
+//  Copyright (c) 2015 HCURA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface panels_workshops_1 : UIViewController <UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *viewWeb;
+
+@end
