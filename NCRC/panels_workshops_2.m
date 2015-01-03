189601839f50c5afdf12853cf194f381a49484fd
Peter Kraft <pkraft@college.harvard.edu>
2015-01-02 20:51:43 -0600

    Panel/workshop descriptions added

--- NCRC/panels_workshops_2.m
+++ NCRC/panels_workshops_2.m
@@ -21,7 +21,7 @@
                               metrics:nil
                               views:NSDictionaryOfVariableBindings(_viewWeb)]];
     //create the string
-    NSMutableString *html = [NSMutableString stringWithString: @"PLACEHOLDER2"];
+    NSMutableString *html = [NSMutableString stringWithString: @"<h3>Workshops: 1 PM</h3><p><b>Innovative Technology</b></p><p>Technology continues to advance at an exponential rate, changing the status quo in almost every industry. Learn about some of the newest and most promising technologies being developed today, and how they might affect you.</p><p><b>Brevia</b></p><p>Brevia is the official publication of the Harvard College Undergraduate Research Association, a forum for science, culture and other big ideas. It is committed to bringing all disciplines of research out of the ivory tower and into the discourse of the interested public. In this workshop, learn more about the publication and how to start a magazine publication at your own school.</p><h3>Panels: 11 AM</h3><p><b>Grad Schol/Felowships</b></p><p>A panel representing diverse perspectives, from administrative to student roles, will discuss graduate school, fellowships, and their application processes.</p><p><b>Joint Degree Programs</b></p><p>Panelists who have pursued joint degrees: MD, PhD, MPH, and MBA will discuss their experiences and joint degree programs.</p><p><b>Research in the Humanities and Social Science</b></p><p>Learn from distinguished researchers in the humanitites about pursuing an academic career in these subjects.</p>"];
     [_viewWeb loadHTMLString:[html description] baseURL:nil];
     _viewWeb.delegate = self;
 }
