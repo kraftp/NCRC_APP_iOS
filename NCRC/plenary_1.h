40cec00a78df960351310eca0a9f1f4ef7d89969
Peter Kraft <pkraft@college.harvard.edu>
2015-01-03 22:45:01 -0600

    added plenary speaker information

---
+++ NCRC/plenary_1.h
@@ -0,0 +1,14 @@
+//
+//  plenary_1.h
+//  NCRC
+//
+//  Created by Peter Kraft on 1/3/15.
+//  Copyright (c) 2015 HCURA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface plenary_1 : UIViewController <UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *viewWeb;
+
+@end
---
+++ NCRC/plenary_2.h
@@ -0,0 +1,14 @@
+//
+//  plenary_2.h
+//  NCRC
+//
+//  Created by Peter Kraft on 1/3/15.
+//  Copyright (c) 2015 HCURA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface plenary_2 : UIViewController <UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *viewWeb;
+
+@end
